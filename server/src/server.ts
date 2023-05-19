3f8386d6b20be9ab052ddd9de46d92a053c6f24d
pauloWeslleY <weslleyspfc@13outlook.com>
2023-05-19 15:43:29 -0300

    feature: BackEnd rotas autenticadas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,22 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app.register(cors, {
   origin: true, // todas URLs de front end poderão acessar nosso back end
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
 })
 
 app
