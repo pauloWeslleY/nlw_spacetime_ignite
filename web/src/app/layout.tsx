b92d7e6571cd458ac1b4c8ffec04646bf9b443d3
pauloWeslleY <weslleyspfc@13outlook.com>
2023-05-21 17:34:44 -0300

    feature: FrontEnd Web GetMemories

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
         <main className="grid min-h-screen grid-cols-2">
+          {/* Right */}
           <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-starts.svg)] bg-cover px-28 py-16">
             {/* Blur */}
             <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
@@ -46,7 +47,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <Copyright />
           </div>
 
-          <div className="flex flex-col bg-[url(../assets/bg-starts.svg)] bg-cover p-16">
+          {/* Right */}
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-starts.svg)] bg-cover">
             {children}
           </div>
         </main>
